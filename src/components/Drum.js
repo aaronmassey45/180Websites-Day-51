393c87ab3b90004dcbcb26cae0045a9ec85180c2
Aaron <aaronmassey45@gmail.com>
2018-06-07 11:47:59 -0500

    Refactored insturments and renamed to Drum

---
+++ src/components/Drum.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Col } from 'react-bootstrap';
+
+import Instrument from './instrument';
+import { keys } from '../keysData';
+
+export default () => {
+  return (
+    <Col sm={6} smOffset={3} className="Instruments">
+      <h2>
+        {keys.map(instrument => {
+          return (
+            <Instrument
+              key={instrument.key}
+              letter={instrument.key}
+              src={instrument.src}
+              data={{ ...instrument }}
+            />
+          );
+        })}
+      </h2>
+    </Col>
+  );
+};
