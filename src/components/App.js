80acbc98787299fc37935557fd151a88fb28f0a4
Aaron <aaronmassey45@gmail.com>
2018-06-07 11:48:38 -0500

    Imported new component

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,39 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col } from 'react-bootstrap'
+import { Grid, Row, Col } from 'react-bootstrap';
 import Navbar from './navbar';
-import Instruments from './instruments';
-import '../App.css';
-
-const keys = ['q', 'w', 'e', 'a', 's', 'd', 'z', 'x', 'c'];
-const sounds = ['Boing', 'Conk', 'Side Stick', 'Snare', 'Closed HH', 'Kick', 'Clap', 'Open HH', 'Kick n Hat'];
+import Drum from './Drum';
 
 export default class App extends Component {
-  handleKeyPress = (e) => {
-    if (keys.includes(e.key)) {
-      const audio = document.getElementById(`${e.key}audio`);
-      audio.play();
-      const index = keys.indexOf(e.key);
-      document.getElementById("viewer").innerHTML = sounds[index];
-    }
-  }
+  // handleKeyPress = e => {
+  //   if (keys.includes(e.key)) {
+  //     const audio = document.getElementById(`${e.key}audio`);
+  //     audio.play();
+  //     const index = keys.indexOf(e.key);
+  //     document.getElementById('viewer').innerHTML = sounds[index];
+  //   }
+  // };
 
   render() {
     return (
-      <div className="App" onKeyPress={this.handleKeyPress} tabIndex="0">
+      <div
+        className="App"
+        onKeyPress={this.handleKeyPress}
+        tabIndex="0"
+        id="drum-machine"
+      >
         <Navbar />
         <div className="container drum-kit">
           <Grid>
             <Row>
-              <Instruments />
-              <Col sm={6}>
-                <Row><h1>The Kyuudaime</h1></Row>
-                <Row>
-                  <Col sm={8} smOffset={2}><h3 id="viewer"> </h3></Col>
-                </Row>
+              <h1>The Kyuudaime</h1>
+            </Row>
+            <Row>
+              <Col sm={8} smOffset={2}>
+                <h3 id="display"> </h3>
               </Col>
+            </Row>
+            <Row>
+              <Drum />
             </Row>
           </Grid>
         </div>
