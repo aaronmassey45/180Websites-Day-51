be7ce890df633ca548ea40f333172b8158116051
Aaron <aaronmassey45@gmail.com>
2018-06-07 11:47:37 -0500

    Moved json to different file

---
+++ src/keysData.js
@@ -0,0 +1,47 @@
+export const keys = [
+  {
+    key: 'Q',
+    src: 'http://free-sounds.net/sound-files/special-effects/BOING_.WAV',
+    sound: 'Boing',
+  },
+  {
+    key: 'W',
+    src: 'http://free-sounds.net/sound-files/special-effects/CONK.WAV',
+    sound: 'Conk',
+  },
+  {
+    key: 'E',
+    src: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3',
+    sound: 'Side Stick',
+  },
+  {
+    key: 'A',
+    src: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3',
+    sound: 'Snare',
+  },
+  {
+    key: 'S',
+    src: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
+    sound: 'Closed HH',
+  },
+  {
+    key: 'D',
+    src: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
+    sound: 'Kick',
+  },
+  {
+    key: 'Z',
+    src: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
+    sound: 'Clap',
+  },
+  {
+    key: 'X',
+    src: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
+    sound: 'Open HH',
+  },
+  {
+    key: 'C',
+    src: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
+    sound: 'Kick n Hat',
+  },
+];
