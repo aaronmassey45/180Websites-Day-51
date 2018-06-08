287a81946385ac18e1a907c0273bb62a4caaf12c
Aaron <aaronmassey45@gmail.com>
2018-06-08 08:41:45 -0500

    Will now update the display when clicked

--- src/components/instrument.js
+++ src/components/instrument.js
@@ -2,16 +2,26 @@ import React, { Component } from 'react';
 import { Col } from 'react-bootstrap';
 
 export default class extends Component {
+  handleClick = () => {
+    const {
+      updateViewer,
+      data: { sound },
+    } = this.props;
+    this.refs[sound].play();
+    updateViewer(sound);
+  };
+
   render() {
-    const { key, src } = this.props.data;
+    const { key, src, sound } = this.props.data;
     return (
-      <Col
-        xs={4}
-        className="drum-pad"
-        id={`${key}`}
-        onClick={() => this.refs.audio.play()}
-      >
-        <audio id={`${key}`} className="clip" src={src} ref="audio" />
+      <Col xs={4} className="drum-pad" id={`${key}`} onClick={this.handleClick}>
+        <audio
+          id={`${key}`}
+          className={`${key}-sound clip`}
+          src={src}
+          ref={sound}
+          data-target={sound}
+        />
         {key}
       </Col>
     );
